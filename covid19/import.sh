07ba042c3bdcfa53481d7da14fc0f948c186d45c
David Chandler <drfib13@gmail.com>
2020-04-01 19:07:29 +0000

    initial checkin

---
+++ covid19/import.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+cd /home/drfib13/covid19
+export today="$(date +"%Y-%m-%d")"
+mkdir $today
+cd $today
+wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-counties.csv
+wget https://raw.githubusercontent.com/nytimes/covid-19-data/master/us-states.csv
+cd ..
+gsutil cp -r "$today" gs://drfib-usc1/covid19/
+bq load --source_format=CSV --autodetect --replace \
+	covid19.us_counties \
+	gs://drfib-usc1/covid19/"$today"/us-counties.csv
+bq load --source_format=CSV --autodetect --replace \
+	covid19.us_states \
+	gs://drfib-usc1/covid19/"$today"/us-states.csv
