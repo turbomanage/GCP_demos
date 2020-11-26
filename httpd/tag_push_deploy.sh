db8f5fbdbbbf3c6869d012977968d9bf9e1e1bb1
David M. Chandler <turbomanage@gmail.com>
2020-11-26 04:52:14 +0000

    fixed DataStuio embed

--- httpd/tag_push_deploy.sh
+++ httpd/tag_push_deploy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Usage: ./tag_push_deploy img_name
 IMG=$1
+SVC=$IMG
 docker tag httpd gcr.io/$DEVSHELL_PROJECT_ID/$IMG
 docker push gcr.io/$DEVSHELL_PROJECT_ID/$IMG
-gcloud run deploy $IMG --image=gcr.io/$DEVSHELL_PROJECT_ID/$IMG --port=80
+gcloud run deploy $SVC --image=gcr.io/$DEVSHELL_PROJECT_ID/$IMG --port=80
