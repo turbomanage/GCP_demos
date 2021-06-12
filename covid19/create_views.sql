9832afc5472bedad3067f69a1c68ba186c4daa6c
David Chandler <turbomanage@gmail.com>
2021-06-12 00:09:49 +0000

    fixed views and such

--- covid19/create_views.sql
+++ covid19/create_views.sql
@@ -1,24 +1,30 @@
 #standardSQL
 CREATE VIEW IF NOT EXISTS covid19.daily_new_cases AS
-SELECT state, county, date,
-first_value(cases) OVER recent as tot_cases,
-cases - lead(cases) OVER recent as new_cases, cases,
-deaths - lead(deaths) OVER recent as new_deaths, deaths
-FROM `cpb100-151023.covid19.us_counties`
-WINDOW recent AS (PARTITION BY state, county ORDER BY date DESC)
-ORDER BY state, county, date DESC
+WITH daily AS 
+(
+	SELECT state, county, date, fips,
+	cases - lead(cases) OVER recent as new_cases, cases,
+	deaths - lead(deaths) OVER recent as new_deaths, deaths
+	FROM covid19.us_counties
+	WINDOW recent AS (PARTITION BY state, county ORDER BY date DESC)
+)
+SELECT *,
+avg(new_cases) OVER rolling AS avg_new_cases_7da,
+avg(new_deaths) OVER rolling AS avg_new_deaths_7da
+FROM daily
+WINDOW rolling AS (PARTITION BY state, county ORDER BY date DESC ROWS BETWEEN CURRENT ROW AND 6 FOLLOWING)
 ;
 CREATE VIEW IF NOT EXISTS covid19.most_recent_totals AS
 WITH most_recent AS (
-SELECT state, county, date,
-row_number() OVER latest as rownum,
-cases - lead(cases) OVER latest as new_cases, 
-deaths - lead(deaths) OVER latest as new_deaths,
-cases as tot_cases,
-deaths as tot_deaths
-FROM `cpb100-151023.covid19.us_counties`
-WINDOW latest AS (PARTITION BY state, county ORDER BY date DESC )
+	SELECT state, county, date, LPAD(CAST(fips AS string),5,"0") AS fips,
+	row_number() OVER latest as rownum,
+	cases - lead(cases) OVER latest as new_cases, 
+	deaths - lead(deaths) OVER latest as new_deaths,
+	cases as tot_cases,
+	deaths as tot_deaths
+	FROM covid19.us_counties
+	WINDOW latest AS (PARTITION BY state, county ORDER BY date DESC )
 )
-SELECT state, county, date, new_cases, tot_cases, new_deaths, tot_deaths
+SELECT state, county, fips, date, new_cases, tot_cases, new_deaths, tot_deaths
 FROM most_recent
 WHERE rownum = 1
--- covid19/per_capita.sql
+++ covid19/per_capita.sql
@@ -1,3 +1,32 @@
 #standardSQL
+CREATE VIEW IF NOT EXISTS covid19.per_capita_totals AS
+WITH daily AS 
+(
+	SELECT state, county, fips, date, new_cases, new_deaths,
+	tot_cases as cases, tot_deaths as deaths,
+	avg(new_cases) OVER rolling AS avg_new_cases_7da,
+	avg(new_deaths) OVER rolling AS avg_new_deaths_7da,
+	FROM covid19.us_totals
+	WINDOW rolling AS (PARTITION BY state, county ORDER BY date DESC ROWS BETWEEN CURRENT ROW AND 6 FOLLOWING)
+)
+SELECT daily.*, c.county_geom,
+  CASE 
+	  WHEN county="New York City" THEN 8399000 
+		  WHEN county="Kansas City" THEN 491918 
+			  ELSE total_pop END as total_pop
+				FROM daily
+				LEFT OUTER JOIN
+				  bigquery-public-data.census_bureau_acs.county_2018_5yr p
+				ON
+				  (daily.fips = p.geo_id)
+				LEFT OUTER JOIN 
+				  `bigquery-public-data.utility_us.us_county_area` c
+				ON
+				  (CAST(daily.fips AS string) = CONCAT(state_fips_code, county_fips_code) )
+				LEFT OUTER JOIN
+				  bigquery-public-data.utility_us.us_states_area s
+				ON
+				  (c.state_fips_code = s.state_fips_code)
+;
 create or replace table covid19.us_totals_per_capita as
 select *, safe_divide(avg_new_deaths_7da,avg_new_cases_7da) as cfr_7da from covid19.per_capita_totals
