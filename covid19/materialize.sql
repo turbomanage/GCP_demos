1f0375788fa688d157a0a0edbb55bfe1ead8142b
David Chandler <turbomanage@gmail.com>
2021-12-13 23:57:59 +0000

    simplified!

--- covid19/materialize.sql
+++ covid19/materialize.sql
@@ -1,8 +1,8 @@
 #standardSQL
 create or replace table covid19.us_daily_cases as
-select * from covid19.daily_new_cases
-	order by state, county, date
+select *,
+lead(avg_new_cases_7da,28) over (PARTITION BY state, county ORDER BY date DESC) as avg_new_cases_7da_lag_28da
+from covid19.daily_new_cases
 ;
 create or replace table covid19.us_totals as
 select * from covid19.most_recent_totals
-	order by state, county
